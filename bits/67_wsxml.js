03e5dcdfaf6e3488004c37b8727af3a944a666b6
Konstantin Kulagin <kkulagin@stiltsoft.com>
2025-06-12 13:37:12 +0300

    refactor: pr fixes

--- bits/67_wsxml.js
+++ bits/67_wsxml.js
@@ -208,8 +208,7 @@ function parse_ws_xml_cols(columns, cols, s) {
 			var style = parseInt(coll.style, 10);
 			var last = colRanges[colRanges.length - 1];
 			if (
-				last &&
-				last.style === style &&
+				last?.style === style &&
 				colm <= last.to + 1
 			) {
 				last.to = Math.max(last.to, colM);
@@ -403,7 +402,7 @@ return function parse_ws_xml_data(sdata/*:string*/, s, opts, guess/*:Range*/, th
 			var style = parseInt(tag.s, 10);
 			var last = rowRanges.at(-1);
 
-			if (last && last.style === style && tagRowIndex <= last.to + 1) {
+			if (last?.style === style && tagRowIndex <= last.to + 1) {
 				last.to = tagRowIndex;
 			} else {
 				rowRanges.push({ from: tagRowIndex, to: tagRowIndex, style: style });
